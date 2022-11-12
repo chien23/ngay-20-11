5187d9e6db745a37790bbb2afa5ce088f2e28b48
chien23 <118019128+chien23@users.noreply.github.com>
2022-11-12 12:58:09 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Thầy Cô';
+const giftUrl = 'https://www.facebook.com/pvchien.dz';
+const eventName = 'Chúc Mừng 20-11';
+const titleCard = 'Happy Vietnamese Teachers';
+const contentCard = '  Nhân ngày Nhà giáo Việt Nam 20/11. Kính chúc thầy/cô và gia đình luôn dồi dào sức khỏe, hạnh phúc và thành đạt trong cuộc sống.';
+
+
+const giftImage = 'z3811310744597_715a25daf961928a2a2865dd7747c1eb.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
